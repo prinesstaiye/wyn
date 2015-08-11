34aefa9037f6a903b843d17396bf01cee259d109
Xavier <xavierchen00@gmail.com>
2015-08-11 19:47:07 -0400

    Added sign up through Parse!

---
+++ js/javascript.js
@@ -0,0 +1,23 @@
+$( document ).ready(function () {
+    Parse.initialize("hrA3EdYBYrNz9SKLtUG0OpSIN5L9L0zQUvDIyLUs", "ALVDecc9XnfGQuCCO3rARwFxIOFSuRjyPkMuOHAp");
+	var TestObject = Parse.Object.extend("TestObject");
+	var testObject = new TestObject();
+	testObject.save({foo: "bar"}).then(function(object) {
+  		alert("yay! it worked");
+	});
+	$("#SignUp").click(function(){
+		var user = new Parse.User();
+		user.set("username", $("#UsernameUp").val() );
+		user.set("password", $("#PasswordUp").val() );
+		user.set("email", $("#EmailUp").val() );
+		user.signUp (null, {
+  			success: function(user) {
+    			console.log("Success!");
+  			},
+  			error: function(user, error) {
+    		// Show the error message somewhere and let the user try again.
+    		alert("Error: " + error.code + " " + error.message); 
+  			}
+  		});
+	});
+});
