85d4ed63d560d14c17cdcdc304cae364f5f8d261
Xavier <xavierchen00@gmail.com>
2015-08-12 12:54:48 -0400

    If User is already logged in, they automatically go to homepage
    also replaced placeholder image :)

--- js/login.js
+++ js/login.js
@@ -5,13 +5,18 @@ $( document ).ready(function () {
 	testObject.save({foo: "bar"}).then(function(object) {
   		alert("yay! it worked");
 	});
+	var currentUser = Parse.User.current();
+if (currentUser) {
+    window.location.replace("homepage.html");
+} else {
+}
 	$("#Login").click(function() {
 		Parse.User.logIn($("#UsernameIn").val(), $("#PasswordIn").val(), {
 	  		success: function(user) {
 	    		window.location.replace("homepage.html");
 	  		},
 	  		error: function(user, error) {
-	    		alert("Error: " + error.code + " " + error.message); 
+	    		alert("Error: " + error.code + " " + error.message); z
 	  		}
 		});
 	})
