d06d060019f34ee41c1d690467791b3c4ec6703b
XavierChen00 <xavierchen00@gmail.com>
2015-08-14 21:53:56 -0400

    Upload image to parse by pressing post. Username is still John Smith

--- js/upload.js
+++ js/upload.js
@@ -5,45 +5,26 @@ $( document ).ready(function () {
 	testObject.save({foo: "bar"}).then(function(object) {
   		alert("yay! it worked");
 	});
-$("#Post").click(function() {
-	  function saveJobApp(objParseFile)
-  {
-     var jobApplication = new Parse.Object("JobApplication");
-     jobApplication.set("applicantName", "Joe Smith");
-     jobApplication.set("profileImg", objParseFile);
-     jobApplication.save(null, 
-     {
-        success: function(gameScore) {
-          // Execute any logic that should take place after the object is saved.
-          var photo = gameScore.get("profileImg");
-          $("#profileImg")[0].src = photo.url();
-        },
-        error: function(gameScore, error) {
-          // Execute any logic that should take place if the save fails.
-          // error is a Parse.Error with an error code and description.
-          alert('Failed to create new object, with error code: ' + error.description);
-        }
-     });
-  }
 
-   
-  	$('#Upload').bind("change", function(e) {
-         var fileUploadControl = $("#Upload")[0];
-         var file = fileUploadControl.files[0];
-         var name = file.name; //This does *NOT* need to be a unique name
-         var parseFile = new Parse.File(name, file);
+$('#Post').bind("click", function(e) {
+     var fileUploadControl = $("#Upload")[0];
+     var file = fileUploadControl.files[0];
+     var filename = file.name; //This does *NOT* need to be a unique name
+     var parseFile = new Parse.File(name, file);
+     
+     parseFile.save().then(function() {
 
-         parseFile.save().then
-         (
-           function() 
-           {
-               saveJobApp(parseFile);
-           }, 
-           function(error) 
-           {
-             alert("error");
-           }
-         );
-  }); 
-});
-});
+     var UploadPost = new Parse.Object("Upload");
+     UploadPost.set("Username", "John Smith");
+     UploadPost.set("Photo", parseFile);
+     UploadPost.save();
+
+    }, function(error) {
+
+      //error handling
+
+    });
+
+}); //end click handler for upload button
+
+});  //end document ready
