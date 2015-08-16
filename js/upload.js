c1ffe7f77cfe65a7658d0268160fa42b36acfb86
XavierChen00 <xavierchen00@gmail.com>
2015-08-15 23:41:14 -0400

    Fixed uploading images

--- js/upload.js
+++ js/upload.js
@@ -1,30 +1,28 @@
 $( document ).ready(function () {
     Parse.initialize("hrA3EdYBYrNz9SKLtUG0OpSIN5L9L0zQUvDIyLUs", "ALVDecc9XnfGQuCCO3rARwFxIOFSuRjyPkMuOHAp");
-	var TestObject = Parse.Object.extend("TestObject");
-	var testObject = new TestObject();
-	testObject.save({foo: "bar"}).then(function(object) {
-  		alert("yay! it worked");
-	});
-
-$('#Post').bind("click", function(e) {
-     var fileUploadControl = $("#Upload")[0];
-     var file = fileUploadControl.files[0];
-     var filename = file.name; //This does *NOT* need to be a unique name
-     var parseFile = new Parse.File(name, file);
-     
-     parseFile.save().then(function() {
-
-     var UploadPost = new Parse.Object("Upload");
-     UploadPost.set("Username", "John Smith");
-     UploadPost.set("Photo", parseFile);
-     UploadPost.save();
-
-    }, function(error) {
-
-      //error handling
-
+    var TestObject = Parse.Object.extend("TestObject");
+    var testObject = new TestObject();
+    testObject.save({foo: "bar"}).then(function(object) {
+        alert("yay! it worked");
     });
 
-}); //end click handler for upload button
+  $('#Post').bind("click", function(e) {
+         var fileUploadControl = $("#profilePhotoFileUpload")[0];
+         var file = fileUploadControl.files[0];
+         var name = file.name; //This does *NOT* need to be a unique name
+         var parseFile = new Parse.File(name, file);
+            
 
-});  //end document ready
+         parseFile.save().then(function saveJobApp (objParseFile) {
+
+     var jobApplication = new Parse.Object("Upload");
+     jobApplication.set("Username", "Joe Smith");
+     jobApplication.set("Photo", objParseFile);
+     jobApplication.save();
+         },
+           function(error) 
+           {
+             alert("error");
+           });
+       });
+  });
