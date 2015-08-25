53342195844dcd0386f5ad4f4265ae019f1ae769
XavierChen00 <xavierchen00@gmail.com>
2015-08-24 23:12:36 -0400

    :-1:

--- www/js/upload.js
+++ www/js/upload.js
@@ -1,25 +1,8 @@
- $( document ).ready(function () {
+ /*$( document ).ready(function () {
     Parse.initialize("hrA3EdYBYrNz9SKLtUG0OpSIN5L9L0zQUvDIyLUs", "ALVDecc9XnfGQuCCO3rARwFxIOFSuRjyPkMuOHAp");
-    var pictureSource;
-    var destinationType;
-
-    document.addEventListener("deviceready",onDeviceReady,false);
-
-    function onDeviceReady()
-    {
-        pictureSource=navigator.camera.PictureSourceType;
-        destinationType=navigator.camera.DestinationType;
-    }
 
 
-    function getPhoto(source) {
-      // Retrieve image file location from specified source
-      navigator.camera.getPicture(onPhotoURISuccess, onFail, { quality: 50,
-        destinationType: destinationType.FILE_URI,
-        sourceType: source });
-    }
-
-  /*$('#Post').bind("click", function(e) {
+  $('#Post').bind("click", function(e) {
         var fileUploadControl = $("#Upload")[0];
          var file = fileUploadControl.files[0];
          var name =  new Date().getTime() + ".jpg"; //This does *NOT* need to be a unique name
@@ -41,8 +24,26 @@
            {
              alert("error:" + JSON.stringify(error));
            });
-       });*/
+       });
 
 
 
-  });
+  });*/
+  var pictureSource;
+  var destinationType;
+
+  document.addEventListener("deviceready",onDeviceReady,false);
+
+  function onDeviceReady()
+  {
+      pictureSource=navigator.camera.PictureSourceType;
+      destinationType=navigator.camera.DestinationType;
+  }
+
+
+  function getPhoto(source) {
+    // Retrieve image file location from specified source
+    navigator.camera.getPicture(onPhotoURISuccess, onFail, { quality: 50,
+      destinationType: destinationType.FILE_URI,
+      sourceType: source });
+  }
