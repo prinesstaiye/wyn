c3b28df35fc9f0c0b6a2c2e0751ba82f246e2b3d
XavierChen00 <xavierchen00@gmail.com>
2015-08-30 19:04:37 -0400

    s

--- www/js/upload.js
+++ www/js/upload.js
@@ -1,5 +1,5 @@
  // $(document).ready(function() {
-Parse.initialize("hrA3EdYBYrNz9SKLtUG0OpSIN5L9L0zQUvDIyLUs", "ALVDecc9XnfGQuCCO3rARwFxIOFSuRjyPkMuOHAp");
+// Parse.initialize("hrA3EdYBYrNz9SKLtUG0OpSIN5L9L0zQUvDIyLUs", "ALVDecc9XnfGQuCCO3rARwFxIOFSuRjyPkMuOHAp");
 			/* var pictureSource;   // picture source
 		 var destinationType; // sets the format of returned value
 
