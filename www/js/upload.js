4964c1a8a8c17fb354530924441c16c99c4e754c
XavierChen00 <xavierchen00@gmail.com>
2015-08-29 20:26:04 -0400

    Maybe

--- www/js/upload.js
+++ www/js/upload.js
@@ -1,105 +1,110 @@
- /*$( document ).ready(function () {
-    Parse.initialize("hrA3EdYBYrNz9SKLtUG0OpSIN5L9L0zQUvDIyLUs", "ALVDecc9XnfGQuCCO3rARwFxIOFSuRjyPkMuOHAp");
-
-
-  $('#Post').bind("click", function(e) {
-        var fileUploadControl = $("#Upload")[0];
-         var file = fileUploadControl.files[0];
-         var name =  new Date().getTime() + ".jpg"; //This does *NOT* need to be a unique name
-         var parseFile = new Parse.File(name, file);
-
-
-         parseFile.save().then(function saveJobApp (objParseFile) {
-
-
-     var jobApplication = new Parse.Object("Upload");
-     jobApplication.set("Question", $("#QuestionText").val());
-     jobApplication.set("Username", Parse.User.current().getUsername());
-     jobApplication.set("Photo", objParseFile);
-     jobApplication.save();
-     alert("Success! You may check your results on your profile page!");
-     location.reload();
-         },
-           function(error)
-           {
-             alert("error:" + JSON.stringify(error));
-           });
-       });
-
-
-
-  });
-  var pictureSource;
-  var destinationType;
-
-  document.addEventListener("deviceready",onDeviceReady,false);
-
-  function onDeviceReady()
-  {
-      pictureSource=navigator.camera.PictureSourceType;
-      destinationType=navigator.camera.DestinationType;
-  }
-
-
-  function getPhoto(source) {
-    // Retrieve image file location from specified source
-    navigator.camera.getPicture(onPhotoURISuccess, onFail, { quality: 50,
-      destinationType: destinationType.FILE_URI,
-      sourceType: source });
-  }
-*/
 Parse.initialize("hrA3EdYBYrNz9SKLtUG0OpSIN5L9L0zQUvDIyLUs", "ALVDecc9XnfGQuCCO3rARwFxIOFSuRjyPkMuOHAp");
+			var pictureSource;   // picture source
+		 var destinationType; // sets the format of returned value
+
+		 // Wait for Cordova to connect with the device
+		 //
+		 document.addEventListener("deviceready",onDeviceReady,false);
+
+		 // Cordova is ready to be used!
+		 //
+		 function onDeviceReady() {
+				 pictureSource=navigator.camera.PictureSourceType;
+				 destinationType=navigator.camera.DestinationType;
+		 }
+
+		 // Called when a photo is successfully retrieved
+		 //
+		 function onPhotoDataSuccess(imageData) {
+			 // Uncomment to view the base64 encoded image data
+			 // console.log(imageData);
+
+			 // Get image handle
+			 //
+			 var smallImage = document.getElementById('smallImage');
+
+			 // Unhide image elements
+			 //
+			 smallImage.style.display = 'block';
+
+			 // Show the captured photo
+			 // The inline CSS rules are used to resize the image
+			 //
+			 smallImage.src = "data:image/jpeg;base64," + imageData;
+		 }
+
+		 // Called when a photo is successfully retrieved
+		 //
+		 function onPhotoURISuccess(imageURI) {
+			 // Uncomment to view the image file URI
+			 // console.log(imageURI);
+
+			 // Get image handle
+			 //
+			 var largeImage = document.getElementById('largeImage');
+
+			 // Unhide image elements
+			 //
+			 largeImage.style.display = 'block';
+
+			 // Show the captured photo
+			 // The inline CSS rules are used to resize the image
+			 //
+			 largeImage.src = imageURI;
+		 }
+
+		 // A button will call this function
+		 //
+		 function capturePhoto() {
+			 // Take picture using device camera and retrieve image as base64-encoded string
+			 navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50,
+				 destinationType: destinationType.DATA_URL });
+		 }
+
+		 // A button will call this function
+		 //
+		 function capturePhotoEdit() {
+			 // Take picture using device camera, allow edit, and retrieve image as base64-encoded string
+			 navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 20, allowEdit: true,
+				 destinationType: destinationType.DATA_URL });
+		 }
+
+		 // A button will call this function
+		 //
+		 function getPhoto(source) {
+			 // Retrieve image file location from specified source
+			 navigator.camera.getPicture(onPhotoURISuccess, onFail, { quality: 50,
+				 destinationType: destinationType.FILE_URI,
+				 sourceType: source });
+		 }
+
+		 // Called if something bad happens.
+		 //
+		 function onFail(message) {
+			 alert('Failed because: ' + message);
+		 }
+
+		  var NoteOb = Parse.Object.extend("Upload");
+		 	var imagedata = "";
+			var noteText = $("#QuestionText").val();
 
 
-function getPhoto(source) {
-  // Retrieve image file location from specified source
-  navigator.camera.getPicture(onPhotoURISuccess, onFail, { quality: 50,
-    destinationType: destinationType.FILE_URI,
-    sourceType: source });
-}
-
-			/*$(document).ready(function(e, ui) {
-
-		var imagedata = "";
-				if(imagedata !== "") {
-					var parseFile = new Parse.File("pic.jpg", {base64:imagedata});
-					console.log(parseFile);
-						parseFile.save().then(function() {
-							var note = new Parse.Object.extend("Upload");
-							note.set("Username", Parse.User.current().getUsername());
-							note.set("Question", $("#QuestionText").val());
-							note.set("Photo",parseFile);
-							note.save(null, {
-								success:function(ob) {
-									alert("success");
-								}, error:function(e) {
-									console.log("Oh crap", e);
-	                alert("error");
-								}
-							});
-
-						}, function(error) {
-							alert("Error");
-							console.log(error);
-						});
-
-				}
-*/
-/* var imagedata = "";
-			$( "#Upload" ).click(function(e) {
-				e.preventDefault();
-				navigator.camera.getPicture(gotPic, failHandler,
-					{quality:50, destinationType:navigator.camera.DestinationType.DATA_URL,
-					 sourceType:navigator.camera.PictureSourceType.PHOTOLIBRARY});
-			});
-
-	function gotPic(data) {
-	console.log('got here');
-	imagedata = data;
-	}
-
-	function failHandler(e) {
-	alert("ErrorFromC");
-	alert(e);
-	console.log(e.toString());
-} */
+		 if(imagedata != "") {
+	 var parseFile = new Parse.File("mypic.jpg", {base64:imagedata});
+	 console.log(parseFile);
+		 parseFile.save().then(function() {
+			 var note = new NoteOb();
+			 note.set("Username", Parse.User.current().getUsername());
+			 note.set("Question",noteText);
+			 note.set("Photo",parseFile);
+			 note.save(null, {
+				 success:function(ob) {
+					location.replace("profile.html");
+				 }, error:function(e) {
+					alert("error");
+				 }
+			 });
+			 });}
+			 else {
+			 	alert("You must add a picture!");
+			 }
